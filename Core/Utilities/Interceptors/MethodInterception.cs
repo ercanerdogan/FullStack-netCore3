cf5e71db02ec8ac9bd271d86b7dea93b01bfd25d
Ercan Erdoğan <ercanerdogan@gmail.com>
2020-01-07 18:06:39 +0300

    Autoface interception structure has been implemented.

---
+++ Core/Utilities/Interceptors/MethodInterception.cs
@@ -0,0 +1,54 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Interceptors
+{
+    public abstract class MethodInterception: MethodInterceptionBaseAttribute
+    {
+        protected virtual void OnBefore(IInvocation invocation)
+        {
+
+        }
+        protected virtual void OnAfter(IInvocation invocation)
+        {
+
+        }
+        protected virtual void OnException(IInvocation invocation)
+        {
+
+        }
+        protected virtual void OnSuccess(IInvocation invocation)
+        {
+
+        }
+
+
+        public override void Intercept(IInvocation invocation)
+        {
+            var isSucces = true;
+            OnBefore(invocation);
+            try
+            {
+                invocation.Proceed();
+            }
+            catch (Exception)
+            {
+                isSucces = false;
+                OnException(invocation);
+                throw;
+            }
+            finally
+            {
+                if (isSucces)
+                {
+                    OnSuccess(invocation);
+
+                }
+            }
+            OnAfter(invocation);
+        }
+
+    }
+}
